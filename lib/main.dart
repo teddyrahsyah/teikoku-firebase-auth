2b5ffece1dd7641e22b24a4c2c2a6dc25dba20af
teddyrahsyah212 <teddyrahsyah@gmail.com>
2022-10-15 10:38:47 +0700

    Add Provider & AuthenticationWrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:teikoku_firebase_auth/firebase_options.dart';
+import 'package:teikoku_firebase_auth/services/auth_services.dart';
 import 'package:teikoku_firebase_auth/ui/views/home_view.dart';
 import 'package:teikoku_firebase_auth/ui/views/login_view.dart';
 import 'package:teikoku_firebase_auth/ui/views/register_view.dart';
@@ -20,10 +23,34 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: LoginView(),
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => AuthService(FirebaseAuth.instance)),
+        StreamProvider(
+          create: (context) => context.read<AuthService>().authStateChange,
+          initialData: null,
+        )
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        home: AuthenticationWrapper(),
+      ),
     );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User?>();
+
+    if (firebaseUser != null) {
+      return const HomeView();
+    }
+
+    return const LoginView();
   }
 }
